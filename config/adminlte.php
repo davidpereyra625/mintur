8b854b4361fbdd2c30f49d29e2a945dac4326fc7
davidpereyra625 <davidpereyra2013.dp@gmail.com>
2023-07-20 11:56:22 -0300

    cambiando la imagen favicon

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,9 +14,9 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => '',
     'title_prefix' => '',
-    'title_postfix' => '',
+    'title_postfix' => ' | Ministerio de Turismo',
 
     /*
     |--------------------------------------------------------------------------
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'use_ico_only' => false,
+    'use_ico_only' => true,
     'use_full_favicon' => false,
 
     /*
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,19 @@
 @extends('adminlte::page')
 
-@section('title', 'AdminLTE')
-
+@section('title', 'Dashboard')
+<!-- en la section title le puse dashboard ya que en el adminlte.php title_prefix ya le ingrese el nombre del ministerio -->
 @section('content_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
+<h1 class="m-0 text-dark">Dashboard</h1>
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <p class="mb-0">esta es la nueva caracteristica en el feature</p>
-                </div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <p class="mb-0">esta es la nueva caracteristica en el feature</p>
             </div>
         </div>
     </div>
+</div>
 @stop
